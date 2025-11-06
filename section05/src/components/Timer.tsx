171e1b84cadf25dfa49abdd7dd7519192bb930ac
Nathan Flynn <Nathan.Flynn@trayport.com>
2025-11-06 18:25:14 +0000

    73 completed

---
+++ section05/src/components/Timer.tsx
@@ -0,0 +1,12 @@
+import Container from './UI/Container.tsx';
+import {type Timer as TimerProps} from '../store/timers-context';
+
+
+export default function Timer({name, duration}: TimerProps) {
+  return (
+    <Container as="article">
+      <h2>{name}</h2>
+        <p>{duration}</p>
+    </Container>
+  );
+}
