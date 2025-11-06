3b88de12d2fb933f8dae23bf740fb4d3910bec06
nf313743 <1612339+nf313743@users.noreply.github.com>
2025-11-06 14:55:00 +0000

    63 completed

--- section04-02/src/App.tsx
+++ section04-02/src/App.tsx
@@ -1,16 +1,20 @@
 import Button from "./components/Button";
 import Input from "./components/Input";
-import Form from "./components/Form";
+import Form, { FormHandle } from "./components/Form";
+import { useRef } from "react";
 
 function App() {
+  const customForm = useRef<FormHandle>(null);
+
   function handleSave(data: unknown) {
     const extractData = data as { name: string; age: string };
     console.log(extractData);
+    customForm.current?.clear();
   }
 
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input type="text" label="Name" id="name" />
         <Input type="number" label="Age" id="age" />
         <p>
--- section04-02/src/components/Form.tsx
+++ section04-02/src/components/Form.tsx
@@ -1,10 +1,34 @@
-import { ComponentPropsWithoutRef, FormEvent } from "react";
+import {
+  ComponentPropsWithoutRef,
+  FormEvent,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from "react";
+
+export type FormHandle = {
+  clear: () => void;
+};
 
 type FormProps = {
   onSave: (value: unknown) => void;
 } & ComponentPropsWithoutRef<"form">;
 
-export default function Form({ onSave, children, ...otherProps }: FormProps) {
+const Form = forwardRef<FormHandle, FormProps>(function Form(
+  { onSave, children, ...otherProps },
+  ref
+) {
+  const form = useRef<HTMLFormElement>(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      clear() {
+        console.log("CLEARING");
+        form.current?.reset();
+      },
+    };
+  });
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
@@ -14,8 +38,10 @@ export default function Form({ onSave, children, ...otherProps }: FormProps) {
   }
 
   return (
-    <form onSubmit={handleSubmit} {...otherProps}>
+    <form onSubmit={handleSubmit} {...otherProps} ref={form}>
       {children}
     </form>
   );
-}
+});
+
+export default Form
